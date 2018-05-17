fc29f9eb3a27d9107feaca079dd3e14a92f73b35
yymirr <abla.ue@web.de>
2018-05-17 11:04:32 +0200

    no more duplicates, for real this time

--- weareone.py
+++ weareone.py
@@ -19,13 +19,16 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 
 # Global variables
-logFilePath = 'some/path' + 'history.log'
+logFilePath = '/home/geeray/.config/weareone/'
+logFile = logFilePath + sys.argv[1] + "_history.log"
 radio = ["technobase", "housetime", "hardbase", "trancebase", "coretime",
          "clubtime","teatime"]
 
-def createHistoryLog():
-    os.system("touch {}".format(logFilePath))
-    print("Created log file: {}".format(logFilePath))
+
+
+# def createHistoryLog():
+#         os.system("touch {}".format(logFile))
+#         print("Created log file: {}".format(logFile))
 
 
 
@@ -38,8 +41,10 @@ class Tracklist():
         self.tracks = []
 
 
-    def getListOfTracks(self):
-
+    def getListOfTracks(self, fname):
+        '''
+        get all the unique tracks..
+        '''
 
         # processing and parsing
         r = requests.get(self.url)
@@ -49,21 +54,20 @@ class Tracklist():
         self.tempList = list(set(list(filter(None,[ x.text for x in links ]))))
 
         # not get rid of duplicates
-        with open(logFilePath) as f:
+        with open(fname) as f:
             content = f.readlines()
         alreadyDownloaded = [track.replace('\n','') for track in content]
-        self.tracks = list(set(self.tempList)^set(alreadyDownloaded))
-
+        self.tracks = [ track for track in self.tempList if track not in alreadyDownloaded ]
         return self.tracks
 
+
     def ytDownTracks(self):
-
-        if not os.path.isfile(logFilePath):
-            createHistoryLog()
-
+        '''
+        ..and keep track of the downloaded songs,by puting their names in a radio specific history file
+        '''
         for track in self.tracks: # gvsearch uses google video, change it to "ytsearch" to use youtube
-            with open(logFilePath, 'a') as f:
-                os.system('youtube-dl --extract-audio --audio-format mp3 -o "{}.%(ext)s" "gvsearch1:{}"'.format(track,track))
+            with open(logFile, 'a') as f:
+                os.system('youtube-dl --extract-audio --audio-format mp3 -o "{}.%(ext)s" "ytsearch1:{}"'.format(track,track))
                 f.write(track + "\n")   # write the track title to log file
 
 
@@ -71,7 +75,8 @@ if __name__== "__main__":
 
     if (len(sys.argv) != 1 and sys.argv[1] not in radio):
         print("Usage: {} {}".format(sys.argv[0],radio))
+        sys.exit(0)
     else:
         tracklist = Tracklist("https://www.{}.fm/tracklist/".format(sys.argv[1]))
-        tracklist.getListOfTracks()
+        tracklist.getListOfTracks(logFile)
         tracklist.ytDownTracks()
